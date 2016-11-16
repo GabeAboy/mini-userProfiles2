ff9c9c0992a42697d9c17fe002802278bcea78b9
Merksue <gabeaboy@gmail.com>
2016-11-16 11:46:24 -0500

    api request

--- controller.js
+++ controller.js
@@ -1,10 +1,20 @@
+// angular.module('userProfiles').controller('MainController', function($scope, mainService) {
+//
+//   $scope.getUsers = function() {
+//   	$scope.users = mainService.getUsers();
+//   };
+//
+//
+//   $scope.getUsers();
+//
+// });
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
-
   $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
+    //enter the service scified function once recieved data then
+    //define $scope.users
+    mainService.getUsers().then(function(response) {
+      $scope.users = response.data.data;
+    });
   }
-
-
   $scope.getUsers();
-
 });
